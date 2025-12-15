baf15a1ea251c153a0b77e035b6d97a292589ae3
SuryaYOHO <https://github.com/SuryaYOHO/Suryaprakash.git>
2025-12-15 19:25:28 +0530

    ss

---
+++ script.js
@@ -0,0 +1,6 @@
+const contactBtn = document.getElementById("contactBtn");
+const msg = document.getElementById("msg");
+
+contactBtn.addEventListener("click", () => {
+  msg.textContent = "Thanks for visiting my profile 😊";
+});
